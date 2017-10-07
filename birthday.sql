785d64222a17d868985e6c58eadb0ada5a471d02
pahan <prispav@gmail.com>
2017-10-08 00:18:31 +0300

    Краткое описание изменений

--- birthday.sql
+++ birthday.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `clients` ADD `birthday` DATETIME NULL AFTER `address`;
-INSERT INTO `menu` (`id`, `parent_id`, `title`, `handler`) VALUES (NULL, '4', 'Дни рождения', 'templates/viewBirthdays.php');
+INSERT INTO `menu` (`id`, `parent_id`, `title`, `handler`) VALUES (NULL, '5', 'Дни рождения', 'templates/viewBirthdays.php');
